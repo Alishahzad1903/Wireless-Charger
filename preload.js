757382402244baecbd16b62d49bfc388c4358e19
haris-mumtazz <harismumtaz07@gmail.com>
2023-07-19 16:10:18 +0800

    Renderer to Main

--- preload.js
+++ preload.js
@@ -1,19 +1,9 @@
-// window.addEventListener('DOMContentLoaded', () => {
-//     const loadingScreen = document.getElementById('loading-screen');
-//     const nextScreen = document.getElementById('next-screen');
-//     const startButton = document.getElementById('start-button');
-  
-//     // Set initial screen to loading
-//     //loadingScreen.classList.add('active');
-  
-//     // Listen for start button click event
-
 
 const { contextBridge, ipcRenderer } = require('electron')
 
 contextBridge.exposeInMainWorld('electronAPI', {
     handlecontroller: (callback) => ipcRenderer.on('update', callback),
-  
-    
+    sendCommand: (value) => ipcRenderer.send('send-command', value),
 })
+
 
