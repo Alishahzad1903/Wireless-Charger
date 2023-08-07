deff9fb95e3e6fe9b362ce9f73487ac97da5635b
haris-mumtazz <harismumtaz07@gmail.com>
2023-08-07 16:40:35 +0800

    Structure Complete

--- Sidebar.js
+++ Sidebar.js
@@ -13,3 +13,20 @@ function changeColor(element) {
     var icon = element.getElementsByTagName('i')[0];
     icon.classList.add('active-icon');
   }
+
+  const radioButtons = document.querySelectorAll('input[type="radio"]');
+
+  radioButtons.forEach((radio) => {
+    radio.addEventListener('change', (event) => {
+      const selectedLabel = event.target.closest('.btn');
+      
+      radioButtons.forEach((radio) => {
+        const label = radio.closest('.btn');
+        if (label !== selectedLabel) {
+          label.classList.remove('selected');
+        }
+      });
+      
+      selectedLabel.classList.toggle('selected', event.target.checked);
+    });
+  });
