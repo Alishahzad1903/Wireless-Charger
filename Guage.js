6a681f86f811ab2c047004848872d82a5a446c6f
haris-mumtazz <harismumtaz07@gmail.com>
2023-08-22 17:30:30 +0800

    Gauge responsiveness

--- Guage.js
+++ Guage.js
@@ -1,5 +1,4 @@
 
-
 window.electronAPI.handlecontroller((event, registerValues) => {
     
   newgauge1 = registerValues[0];
@@ -17,76 +16,78 @@ var ctx = document.getElementById('gaugeChart').getContext('2d');
 createvoltageChart();
 
 function createvoltageChart() {
+  
   var data = {
-      datasets: [{ 
-          data: [0, 100-0],
-          backgroundColor: ['#FF5733','#d6d4d0'],
-          borderWidth: 0
-      }]
+    datasets: [{ 
+      data: [0, 100-0],
+      backgroundColor: ['#FF5733','#d6d4d0'],
+      borderWidth: 0
+    }]
   };
-  var voltageoptions={
-        aspectRatio: 1.5,
-          rotation: 270,
-          circumference: 180,
-          responsive: true,
-          maintainAspectRatio: false,  //Add this line to make the chart responsive
-          borderWidth: 1,
-          cutout: '85%',
-          plugins: {
-            legend: {
-              display: false,
-            },
-            tooltip: {
-              enabled: false,
-            },
-          },
-          layout: {
-            padding: {
-              top: 20,
-              bottom: 20,
-              left: 20,
-              right: 20,
-            },
-          },
 
+  var voltageoptions={
+    aspectRatio: 1.7,
+    rotation: 270,
+    circumference: 180,
+    responsive: true,
+    maintainAspectRatio: true,  //Add this line to make the chart responsive
+    borderWidth: 1,
+    cutout: '90%',
+    plugins: {
+      legend: {
+        display: false,
+      },
+      tooltip: {
+        enabled: false,
+      },
+    },
+    layout: {
+      padding: {
+        top: 5,
+        bottom: 10,
+        left: 15,
+        right: 15,
+      },
+    },
   }
 
   var guaguecharttext={
-      id:'guaguecharttext',
-      afterDatasetsDraw(chart,args,pluginOptions){
-          const{ctx,data,chartArea:{top,bottom,left,right,width,height},scales:{r}}=chart;
-          ctx.save();
-          const X=chart.getDatasetMeta(0).data[0].x;
-          const Y=chart.getDatasetMeta(0).data[0].y;
-          
-          ctx.font='15px MyCustomFont2';
-          ctx.fillStyle = 'rgba(255, 255, 255, 0.75)';
-          ctx.textBaseLine='bottom';
-          ctx.textAlign='left';
-          ctx.fillText('-1000',left+0.05*width,Y+15);
-          ctx.textAlign='right';
-          ctx.fillText('1000',right-0.07*width,Y+15);
+    id:'guaguecharttext',
+    afterDatasetsDraw(chart,args,pluginOptions){
+      const{ctx,data,chartArea:{top,bottom,left,right,width,height},scales:{r}}=chart;
+      ctx.save();
+      const X=chart.getDatasetMeta(0).data[0].x;
+      const Y=chart.getDatasetMeta(0).data[0].y;
+      
+      ctx.font='14px MyCustomFont2';
+      ctx.fillStyle = 'rgba(255, 255, 255, 0.75)';
+      ctx.textBaseLine='bottom';
+      ctx.textAlign='left';
+      ctx.fillText('-1000',left-0.03*width,Y+15);
+      ctx.textAlign='right';
+      ctx.fillText('1000',right+0.02*width,Y+15);
 
-          ctx.font='35px MyCustomFont2';
-          ctx.textAlign='center';
-          ctx.textBaseLine='top';
-          const score=data.datasets[0].data[0];
-          ctx.fillStyle = 'rgba(255, 255, 255, 1)';
-          ctx.fillText(score,X,Y-0.3*height);
+      ctx.font='40px MyCustomFont2';
+      ctx.textAlign='center';
+      ctx.textBaseLine='top';
+      const score=data.datasets[0].data[0];
+      ctx.fillStyle = 'rgba(255, 255, 255, 1)';
+      ctx.fillText(score,X,Y-0.3*height);
 
 
-          ctx.font='20px MyCustomFont2';
-          ctx.fillStyle = 'rgba(255, 255, 255, 0.75)';
-          ctx.fillText('Var 1',X,Y)
-          ctx.textAlign='center';
+      ctx.font='25px MyCustomFont2';
+      ctx.fillStyle = 'rgba(255, 255, 255, 0.75)';
+      ctx.textAlign='center';
+      ctx.fillText('Var 1',X,Y)
 
-      }
+    }
   }
+
   gaugevoltage=new Chart(ctx, {
-      type: 'doughnut',
-      data: data,
-      options: voltageoptions,
-      plugins:[guaguecharttext]
+    type: 'doughnut',
+    data: data,
+    options: voltageoptions,
+    plugins:[guaguecharttext]
   });
 
 
--- Guagecurrent.js
+++ Guagecurrent.js
@@ -1,5 +1,4 @@
 
-
 window.electronAPI.handlecontroller((event, registerValues) => {
     
   newgauge1 = registerValues[1];
@@ -17,84 +16,84 @@ var ctxcurrent=document.getElementById('gaugeChartcurrent').getContext('2d');
 createcurrentChart();
 
 function createcurrentChart() {
-    var datacurrent = {
-        datasets: [{ 
-            data: [0, 100-0],
-            backgroundColor: ['#33FF57','#d6d4d0'],
-            borderWidth: 0
-        }]
+
+  var datacurrent = {
+    datasets: [{ 
+      data: [0, 100-0],
+      backgroundColor: ['#33FF57','#d6d4d0'],
+      borderWidth: 0
+    }]
+  };
+
+  var currentoptions = {
+    aspectRatio: 1.7,
+    rotation: 270,
+    circumference: 180,
+    responsive: true,
+    maintainAspectRatio: true,  
+    borderWidth: 1,
+    cutout: '90%',
+    plugins: {
+      legend: {
+        display: false,
+      },
+      tooltip: {
+        enabled: false,
+      },
+    },
+    layout: {
+      padding: {
+        top: 5,
+        bottom: 15,
+        left: 15,
+        right: 15,
+      },
+    },
+  };
+
+  var gaugecurrenttext={
+    id:'guaguecharttext',
+    afterDatasetsDraw(chart,args,pluginOptions){
+      const{ctx,data,chartArea:{top,bottom,left,right,width,height},scales:{r}}=chart;
+      ctx.save();
+      const X=chart.getDatasetMeta(0).data[0].x;
+      const Y=chart.getDatasetMeta(0).data[0].y;
+      
+      ctx.font='14px MyCustomFont2';
+      ctx.fillStyle = 'rgba(255, 255, 255, 0.75)';
+      ctx.textBaseLine='bottom';
+      ctx.textAlign='left';
+      ctx.fillText('-1000',left-0.03*width,Y+15);
+      ctx.textAlign='right';
+      ctx.fillText('1000',right+0.02*width,Y+15);
+
+      ctx.font='40px MyCustomFont2';
+      ctx.textAlign='center';
+      ctx.textBaseLine='top';
+      const score=data.datasets[0].data[0];
+      ctx.fillStyle = 'rgba(255, 255, 255, 1)';
+      ctx.fillText(score,X,Y-0.3*height);
+
+
+      ctx.font='25px MyCustomFont2';
+      ctx.fillStyle = 'rgba(255, 255, 255, 0.75)';
+      ctx.fillText('Var 1',X,Y)
+      ctx.textAlign='center';
+        
+      },
     };
-    var currentoptions = {
-        aspectRatio: 1.5,
-        rotation: 270,
-        circumference: 180,
-        responsive: true,
-        maintainAspectRatio: false,  //Add this line to make the chart responsive
-        borderWidth: 1,
-        cutout: '85%',
-        plugins: {
-          legend: {
-            display: false,
-          },
-          tooltip: {
-            enabled: false,
-          },
-        },
-        layout: {
-          padding: {
-            top: 20,
-            bottom: 20,
-            left: 20,
-            right: 20,
-          },
-        },
-        };
-        const gaugecurrenttext={
-                  id:'gaugecurrenttext',
-                  afterDatasetsDraw(chart,args,pluginOptions){
-                      const{ctx,data,chartArea:{top,bottom,left,right,width,height},scales:{r}}=chart;
-                      ctx.save();
-                      const X=chart.getDatasetMeta(0).data[0].x;
-                      const Y=chart.getDatasetMeta(0).data[0].y;
-                      
-                      ctx.font='15px MyCustomFont2';
-                      ctx.fillStyle = 'rgba(255, 255, 255, 0.75)';
-                      ctx.textBaseLine='bottom';
-                      ctx.textAlign='left';
-                      ctx.fillText('-1000',left+0.05*width,Y+15);
-                      ctx.textAlign='right';
-                      ctx.fillText('1000',right-0.07*width,Y+15);
-
-                      ctx.font='35px MyCustomFont2';
-                      ctx.textAlign='center';
-                      ctx.textBaseLine='top';
-                      const score=data.datasets[0].data[0];
-                      ctx.fillStyle = 'rgba(255, 255, 255, 1)';
-                      ctx.fillText(score,X,Y-0.3*height);
-
-
-                      ctx.font='20px MyCustomFont2';
-                      ctx.fillStyle = 'rgba(255, 255, 255, 0.75)';
-                      ctx.fillText('Var 2',X,Y)
-                      ctx.textAlign='center';
-            
-              }
-              }
-              gaugecurrent=new Chart(ctxcurrent, {
-                  type: 'doughnut',
-                  data: datacurrent,
-                  options: currentoptions,
-                  plugins:[gaugecurrenttext]
-              });
-
+      
+  gaugecurrent=new Chart(ctxcurrent, {
+    type: 'doughnut',
+    data: datacurrent,
+    options: currentoptions,
+    plugins:[gaugecurrenttext]
+  });
 
 }
 
 function updatecurrentChart() {
-    gaugecurrent.data.datasets[0].data = [newgauge1, 100 - newgauge1];
-    gaugecurrent.update();
-  }
+  gaugecurrent.data.datasets[0].data = [newgauge1, 100 - newgauge1];
+  gaugecurrent.update();
+}
 
-
-
-//make changes
